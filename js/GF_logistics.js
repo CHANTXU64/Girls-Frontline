d08ba8d1981863110ab1c421125369105266cac9
ChantXu64 <chantxu@outlook.com>
2019-10-07 00:09:32 +0800

    update method3

--- js/GF_logistics.js
+++ js/GF_logistics.js
@@ -11,55 +11,55 @@ function Weights(Target, Current){
     else return (10 + (Target - Current) / 4) * (Target - Current);
 }
 
-var Q = [//后勤战役-人力 弹药 口粮 零件 人形 装备 快建 时间(分钟)
-    [0,       174,     174,     0,       0, 0, 0, 50],//0-1
-    [183.333, 0,       0,       116.667, 0, 0, 0, 180],
-    [75,      75,      75,      20.833,  0, 0, 0, 720],
-    [0,       50,      33.333,  31.25,   0, 0, 0, 1440],
-    [40,      120,     60,      0,       0, 0, 0, 15],//1-1
-    [0,       80,      120,     0,       0, 0, 0, 30],
-    [30,      0,       30,      10,      0, 0, 0, 60],
-    [80,      80,      0,       0,       0, 0, 0, 120],
-    [150,     0,       0,       45,      0, 0, 0, 40],//2-1
-    [40,      133.33,  53.333,  0,       0, 0, 0, 90],
-    [2.5,     2.5,     2.5,     57.5,    0, 0, 0, 240],
-    [0,       41.667,  100,     10,      0, 0, 0, 360],
-    [150,     0,       225,     0,       0, 0, 0, 20],//3-1
-    [0,       160,     93.333,  40,      0, 0, 0, 45],
-    [0,       200,     0,       0,       0, 0, 0, 90],
-    [0,       0,       60,      60,      0, 0, 0, 300],
-    [0,       185,     185,     0,       0, 0, 0, 60],//4-1
-    [0,       0,       0,       105,     0, 0, 0, 120],
-    [133.333, 91.667,  0,       0,       0, 0, 0, 360],
-    [50,      50,      50,      18.75,   0, 0, 0, 480],
-    [0,       0,       200,     90,      0, 0, 0, 30],//5-1
-    [0,       240,     120,     0,       0, 0, 0, 150],
-    [200,     100,     100,     0,       0, 0, 0, 240],
-    [14.286,  0,       0,       100,     0, 0, 0, 420],
-    [150,     150,     0,       50,      0, 0, 0, 120],//6-1
-    [0,       66.667,  183.333, 33.333,  0, 0, 0, 180],
-    [0,       0,       40,      100,     0, 0, 0, 300],
-    [66.667,  66.667,  66.667,  0,       0, 0, 0, 720],
-    [260,     0,       260,     0,       0, 0, 0, 150],//7-1
-    [0,       162.5,   0,       75,      0, 0, 0, 240],
-    [163.636, 109.09,  109.09,  0,       0, 0, 0, 330],
-    [31.25,   31.25,   31.25,   75,      0, 0, 0, 480],
-    [150,     150,     150,     0,       0, 0, 0, 60],//8-1
-    [0,       0,       0,       150,     0, 0, 0, 180],
-    [66.667,  133.333, 133.333, 0,       0, 0, 0, 360],
-    [166.667, 44.444,  44.444,  11.111,  0, 0, 0, 540],
-    [0,       0,       200,     100,     0, 0, 0, 30],//9-1
-    [120,     0,       120,     66.667,  0, 0, 0, 90],
-    [166.667, 166.667, 0,       0,       0, 0, 0, 270],
-    [71.423,  128.571, 128.571, 0,       0, 0, 0, 420],
-    [210,     300,     0,       0,       0, 0, 0, 40],//10-1
-    [0,       144,     108,     0,       0, 0, 0, 100],
-    [0,       90,      90,      56.25,   0, 0, 0, 320],
-    [66,      66,      66,      33,      0, 0, 0, 600],
-    [87.5,    262.5,   0,       0,       0, 0, 0, 240],//11-1
-    [90,      135,     135,     0,       0, 0, 0, 240],
-    [0,       93.75,   187.5,   31.25,   0, 0, 0, 480],
-    [0,       165,     0,       90,      0, 0, 0, 600]];
+var Q = [//后勤战役-人力 弹药 口粮 零件 人形 装备 快建 时间(hour)
+    [0,       174,     174,     0,       0, 0, 0, 0.8333],//0-1
+    [183.333, 0,       0,       116.667, 0, 0, 0, 3],
+    [75,      75,      75,      20.833,  0, 0, 0, 12],
+    [0,       50,      33.333,  31.25,   0, 0, 0, 24],
+    [40,      120,     60,      0,       0, 0, 0, 0.25],//1-1
+    [0,       80,      120,     0,       0, 0, 0, 0.5],
+    [30,      0,       30,      10,      0, 0, 0, 1],
+    [80,      80,      0,       0,       0, 0, 0, 2],
+    [150,     0,       0,       45,      0, 0, 0, 0.6666],//2-1
+    [40,      133.33,  53.333,  0,       0, 0, 0, 1.5],
+    [2.5,     2.5,     2.5,     57.5,    0, 0, 0, 4],
+    [0,       41.667,  100,     10,      0, 0, 0, 6],
+    [150,     0,       225,     0,       0, 0, 0, 0.3333],//3-1
+    [0,       160,     93.333,  40,      0, 0, 0, 0.75],
+    [0,       200,     0,       0,       0, 0, 0, 1.5],
+    [0,       0,       60,      60,      0, 0, 0, 5],
+    [0,       185,     185,     0,       0, 0, 0, 1],//4-1
+    [0,       0,       0,       105,     0, 0, 0, 2],
+    [133.333, 91.667,  0,       0,       0, 0, 0, 6],
+    [50,      50,      50,      18.75,   0, 0, 0, 8],
+    [0,       0,       200,     90,      0, 0, 0, 0.5],//5-1
+    [0,       240,     120,     0,       0, 0, 0, 2.5],
+    [200,     100,     100,     0,       0, 0, 0, 4],
+    [14.286,  0,       0,       100,     0, 0, 0, 7],
+    [150,     150,     0,       50,      0, 0, 0, 2],//6-1
+    [0,       66.667,  183.333, 33.333,  0, 0, 0, 3],
+    [0,       0,       40,      100,     0, 0, 0, 5],
+    [66.667,  66.667,  66.667,  0,       0, 0, 0, 12],
+    [260,     0,       260,     0,       0, 0, 0, 2.5],//7-1
+    [0,       162.5,   0,       75,      0, 0, 0, 4],
+    [163.636, 109.09,  109.09,  0,       0, 0, 0, 5.5],
+    [31.25,   31.25,   31.25,   75,      0, 0, 0, 8],
+    [150,     150,     150,     0,       0, 0, 0, 1],//8-1
+    [0,       0,       0,       150,     0, 0, 0, 3],
+    [66.667,  133.333, 133.333, 0,       0, 0, 0, 6],
+    [166.667, 44.444,  44.444,  11.111,  0, 0, 0, 9],
+    [0,       0,       200,     100,     0, 0, 0, 0.5],//9-1
+    [120,     0,       120,     66.667,  0, 0, 0, 1.5],
+    [166.667, 166.667, 0,       0,       0, 0, 0, 4.5],
+    [71.423,  128.571, 128.571, 0,       0, 0, 0, 7],
+    [210,     300,     0,       0,       0, 0, 0, 0.6666],//10-1
+    [0,       144,     108,     0,       0, 0, 0, 1.6666],
+    [0,       90,      90,      56.25,   0, 0, 0, 5.3333],
+    [66,      66,      66,      33,      0, 0, 0, 10],
+    [87.5,    262.5,   0,       0,       0, 0, 0, 4],//11-1
+    [90,      135,     135,     0,       0, 0, 0, 4],
+    [0,       93.75,   187.5,   31.25,   0, 0, 0, 8],
+    [0,       165,     0,       90,      0, 0, 0, 10]];
 var Plan_length = 36;//Plan.lenth
 var Plan = new Array(Plan_length);//方案
 for (var i = 0; i < Plan.length; i++){
@@ -75,6 +75,7 @@ var Addition_rate;//大成功加成率
 var ProbUP = 0;//大成功UP
 var method = 1;
 var hour_or_day = "hour";
+var One_cycle_time;
 
 //监听标签页
 $(function (){
@@ -266,6 +267,7 @@ function Plan_Push(n1, n2, n3, n4) {
             Plan[ii - 1][6] = RC;
             Plan[ii - 1][7] = PC;
             Plan[ii - 1][8] = Plan_value;
+            if (method === 3) Plan[ii - 1][9] = One_cycle_time;
             break;
         }//方案列表未满 
         if (Plan_value >= Plan[ii - 1][8]) continue;
@@ -280,6 +282,7 @@ function Plan_Push(n1, n2, n3, n4) {
                 Plan[iii - 1][6] = Plan[iii - 2][6];
                 Plan[iii - 1][7] = Plan[iii - 2][7];
                 Plan[iii - 1][8] = Plan[iii - 2][8];
+                if (method === 3) Plan[iii - 1][9] = Plan[iii - 2][9];
             }
             Plan[ii - 1][0] = n1;
             Plan[ii - 1][1] = n2;
@@ -290,6 +293,7 @@ function Plan_Push(n1, n2, n3, n4) {
             Plan[ii - 1][6] = RC;
             Plan[ii - 1][7] = PC;
             Plan[ii - 1][8] = Plan_value;
+            if (method === 3) Plan[ii - 1][9] = One_cycle_time;
             //改变方案
             break;
         }
@@ -390,6 +394,7 @@ function getPlan_1() {
                         }
                     }
                     if (conti_4 === true) continue;
+
                     if (hour_or_day === "hour") {
                         MC = Addition_rate * (Q[n1 - 1][0] + Q[n2 - 1][0] + Q[n3 - 1][0] + Q[n4 - 1][0]);
                         AC = Addition_rate * (Q[n1 - 1][1] + Q[n2 - 1][1] + Q[n3 - 1][1] + Q[n4 - 1][1]);
@@ -455,7 +460,7 @@ function getPlan_2() {
     Hours = Time_hours + Time_minutes / 60;
     //排除超时后勤
     for (var i = 1; i <= Q.length; i++) {
-        if (Q[i - 1][7] > (Hours * 60)) {
+        if (Q[i - 1][7] > Hours) {
             if (Block_Numbers.indexOf(i) == -1) {
                 Block_Numbers.push(i);
             }
@@ -499,10 +504,10 @@ function getPlan_2() {
                     }
                     if (conti_4 === true) continue;
 
-                    MC = Addition_rate * (Q[n1-1][0]*Q[n1-1][7]/60 + Q[n2-1][0]*Q[n2-1][7]/60 + Q[n3-1][0]*Q[n3-1][7]/60 + Q[n4-1][0]*Q[n4-1][7]/60);
-                    AC = Addition_rate * (Q[n1-1][1]*Q[n1-1][7]/60 + Q[n2-1][1]*Q[n2-1][7]/60 + Q[n3-1][1]*Q[n3-1][7]/60 + Q[n4-1][1]*Q[n4-1][7]/60);
-                    RC = Addition_rate * (Q[n1-1][2]*Q[n1-1][7]/60 + Q[n2-1][2]*Q[n2-1][7]/60 + Q[n3-1][2]*Q[n3-1][7]/60 + Q[n4-1][2]*Q[n4-1][7]/60);
-                    PC = Addition_rate * (Q[n1-1][3]*Q[n1-1][7]/60 + Q[n2-1][3]*Q[n2-1][7]/60 + Q[n3-1][3]*Q[n3-1][7]/60 + Q[n4-1][3]*Q[n4-1][7]/60);
+                    MC = Addition_rate * (Q[n1-1][0] * Q[n1-1][7] + Q[n2-1][0] * Q[n2-1][7] + Q[n3-1][0] * Q[n3-1][7] + Q[n4-1][0] * Q[n4-1][7]);
+                    AC = Addition_rate * (Q[n1-1][1] * Q[n1-1][7] + Q[n2-1][1] * Q[n2-1][7] + Q[n3-1][1] * Q[n3-1][7] + Q[n4-1][1] * Q[n4-1][7]);
+                    RC = Addition_rate * (Q[n1-1][2] * Q[n1-1][7] + Q[n2-1][2] * Q[n2-1][7] + Q[n3-1][2] * Q[n3-1][7] + Q[n4-1][2] * Q[n4-1][7]);
+                    PC = Addition_rate * (Q[n1-1][3] * Q[n1-1][7] + Q[n2-1][3] * Q[n2-1][7] + Q[n3-1][3] * Q[n3-1][7] + Q[n4-1][3] * Q[n4-1][7]);
                     Plan_value = Value();
                     Plan_Push(n1, n2, n3, n4);
                 }
@@ -514,8 +519,9 @@ function getPlan_2() {
 
 function initialize_3() {
     Hours = 0;
+    One_cycle_time = 0;
     for (var i = 0; i < Plan_length; i++){
-        for (var ii = 0; ii < 9; ii++){
+        for (var ii = 0; ii < 10; ii++){
             Plan[i][ii] = 0;
         }
     }
@@ -549,6 +555,102 @@ function initialize_3() {
     Block_Numbers = Block_Maps(parseFloat(document.getElementById('MapLimit').value));
 }
 
+//求一个数组的最小公倍数
+//my.oschina.net/tearlight/blog/145135
+//by: subying
+function gcd(a,b){
+    var minNum = Math.min(a,b),maxNum = Math.max(a,b),i=Math.floor(maxNum/minNum),vper=0;
+    if(a ===0 || b===0){
+        return maxNum;
+    }
+
+    for(;i<=maxNum;i++){
+        vper = minNum * i;
+        if(vper % maxNum === 0){
+            return vper;
+            break;
+        }
+    }
+}
+function gcds(arr){
+    var onum = 0,i = 0,len = arr.length,midNum = 0;
+    for(;i<len;i++){
+        onum = Math.floor(arr[i]);//去掉小数
+        midNum = gcd(midNum,onum);
+    }
+    return midNum;
+}
+
+function getPlan_3() {
+    initialize_3();
+    var Time_hours = parseFloat(document.getElementById('Time3_hours').value);
+    var Time_minutes = parseFloat(document.getElementById('Time3_minutes').value);
+    Hours = Time_hours + Time_minutes / 60;
+    var conti_1, conti_2, conti_3, conti_4;
+    for (var n1 = 1; n1 <= (Q.length - 3); n1++) {
+        conti_1 = false;
+        for (var ii = 1; ii <= Block_Numbers.length; ii++) {
+            if (n1 === Block_Numbers[ii-1]) {
+                conti_1 = true;
+                break;
+            }
+        }
+        if (conti_1 === true) continue;
+        for (var n2 = n1 + 1; n2 <= (Q.length - 2); n2++) {
+            conti_2 = false;
+            for (var ii = 1; ii <= Block_Numbers.length; ii++) {
+                if (n2 === Block_Numbers[ii-1]) {
+                    conti_2 = true;
+                    break;
+                }
+            }
+            if (conti_2 === true) continue;
+            for (var n3 = n2 + 1; n3 <= (Q.length - 1); n3++) {
+                conti_3 = false;
+                for (var ii = 1; ii <= Block_Numbers.length; ii++) {
+                    if (n3 === Block_Numbers[ii-1]) {
+                        conti_3 = true;
+                        break;
+                    }
+                }
+                if (conti_3 === true) continue;
+                for (var n4 = n3 + 1; n4 <= Q.length; n4++) {
+                    conti_4 = false;
+                    for (var ii = 1; ii <= Block_Numbers.length; ii++) {
+                        if (n4 === Block_Numbers[ii-1]) {
+                            conti_4 = true;
+                            break;
+                        }
+                    }
+                    if (conti_4 === true) continue;
+
+                    var times = [1, 1, 1, 1];
+                    var number = [n1, n2, n3, n4];
+                    var MC_n = [0, 0, 0, 0];
+                    var AC_n = [0, 0, 0, 0];
+                    var RC_n = [0, 0, 0, 0];
+                    var PC_n = [0, 0, 0, 0];
+                    for (var i = 0; i < 4; i++) {
+                        while (times[i] * Hours < Q[number[i] - 1][7]) times[i] += 1;
+                        MC_n[i] = Q[number[i] - 1][0] * Q[number[i] - 1][7] / (times[i] * Hours);
+                        AC_n[i] = Q[number[i] - 1][1] * Q[number[i] - 1][7] / (times[i] * Hours);
+                        RC_n[i] = Q[number[i] - 1][2] * Q[number[i] - 1][7] / (times[i] * Hours);
+                        PC_n[i] = Q[number[i] - 1][3] * Q[number[i] - 1][7] / (times[i] * Hours);
+                    }
+                    MC = MC_n[0] + MC_n[1] + MC_n[2] + MC_n[3];
+                    AC = AC_n[0] + AC_n[1] + AC_n[2] + AC_n[3];
+                    RC = RC_n[0] + RC_n[1] + RC_n[2] + RC_n[3];
+                    PC = PC_n[0] + PC_n[1] + PC_n[2] + PC_n[3];
+                    One_cycle_time = gcds(times) * Hours;
+                    Plan_value = Value();
+                    Plan_Push(n1, n2, n3, n4);
+                }
+            }
+        }
+    }
+    Print_Table();
+}
+
 window.onload = function () {
     var Table = document.getElementById("Plan_Table");
     var tab = '点击 <span style="color:deepskyblue">开始排序</span> 按钮，排序前三十六的结果将在这里显示';
@@ -567,6 +669,7 @@ function Print_Table(){
             tab = '<table class="table table-striped table-bordered table-hover"><thead><tr><th class="col-0.3">#</th><th>关卡1</th><th>关卡2</th><th>关卡3</th><th>关卡4</th><th>人力/h</th><th>弹药/h</th><th>口粮/h</th><th>零件/h</th><th>人形/h</th><th>装备/h</th><th>快建/h</th></tr></thead>';
             tab += '<tbody>'; break;
         case 3:
+            tab = '<table class="table table-striped table-bordered table-hover"><thead><tr><th class="col-0.3">#</th><th>关卡1</th><th>关卡2</th><th>关卡3</th><th>关卡4</th><th>人力/h</th><th>弹药/h</th><th>口粮/h</th><th>零件/h</th><th>人形/h</th><th>装备/h</th><th>快建/h</th><th><a href="#" data-toggle="tooltip" data-placement="top" title="一周期指从全部后勤同时开始到下一次全部后勤同时收取这段时间">一个周期</a></th></tr></thead>';
             tab += '<tbody>'; break;
     }
     for (var i = 1; i <= Plan.length; i++) {
@@ -589,8 +692,10 @@ function Print_Table(){
                 tab += ("<td>" + (Math.round(Math.max(Q[Plan[i-1][0]-1][7],Q[Plan[i-1][1]-1][7],Q[Plan[i-1][2]-1][7],Q[Plan[i-1][3]-1][7]) / 0.6) / 100) + "h</td></tr>");
                 break;
             case 3:
+                tab += ("<td>" + (Math.round(Plan[i - 1][9] * 10) / 10) + "(h)</td>");
                 break;
         }
     }
     Table.innerHTML = tab;
+    $(function (){$("[data-toggle='tooltip']").tooltip();})
 }
