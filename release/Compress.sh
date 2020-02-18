be0843ddb4066c92fb81dec9d8367f150296f0dd
CHANTXU64 <chantxu@outlook.com>
2020-02-18 16:41:41 +0800

    compress

--- release/Compress.sh
+++ release/Compress.sh
@@ -1,87 +1,63 @@
-rm ./js/GF_logistics.min.js
+#先通过MergeJS.sh合并js，然后手动删除测试代码//test[\w|\W]+?//End test, 再运行该脚本
+
+version="v1_0_0"
+
+date=$(date "+%m%d")_$version
+jsfile="GFLGSTS_"$date".js"
+jsfile_min="GFLGSTS_"$date".min.js"
+jsfile_map=$jsfile_min".map"
+cssdependentfile="GFLGSTS_dependent_"$date".min.css"
+jsdependentfile="GFLGSTS_dependent_"$date".min.js"
+cssfile="GFLGSTS_"$date".min.css"
+
+rm *.html
+rm ./js/*
+rm ./css/*
+rm ./dependent/css/*
+rm ./dependent/js/*
+
+#dependent
+cat ../vendor/bootstrap/css/bootstrap.min.css > ./dependent/css/$cssdependentfile
+awk '{print $0}' ../vendor/jquery-v2.1.1/jquery.min.js ../vendor/bootstrap/js/bootstrap.min.js ../vendor/echarts/echarts.min.js ../vendor/html2canvas/html2canvas.min.js ../vendor/js-sha1/sha1.min.js ../vendor/js-md5/md5.min.js ../vendor/lz-string/lz-string.min.js > ./dependent/js/$jsdependentfile
+
+#css
+java -jar closure-stylesheets.jar ../css/GF_logistics.css > ./css/$cssfile
+
+#js
 echo "{\"presets\":[\"es2015\"],\"plugins\":[]}" > .babelrc
-cp -r ../js/* ./
-cp -r ../lang/* ./
-babel ./GF_logistics_function.js -o ./1.js
-rm ./GF_logistics_function.js
-babel ./GF_logistics_input.js -o ./2.js
-rm ./GF_logistics_input.js
-babel ./Quick_Sort.js -o ./3.js
-rm ./Quick_Sort.js
-babel ./languages.js -o ./4.js
-rm ./languages.js
-babel ./GF_logistics_localStorage.js -o ./5.js
-rm ./GF_logistics_localStorage.js
-babel ./GF_logistics_loadHTML.js -o ./6.js
-rm ./GF_logistics_loadHTML.js
-babel ./GF_logistics_html.js -o ./7.js
-rm ./GF_logistics_html.js
-babel ./GF_logistics_htmlEvent.js -o ./8.js
-rm ./GF_logistics_htmlEvent.js
-babel ./GF_logistics_data.js -o ./9.js
-rm ./GF_logistics_data.js
-babel ./GF_logistics_Tab.js -o ./10.js
-rm ./GF_logistics_Tab.js
-babel ./GF_logistics_Plan.js -o ./11.js
-rm ./GF_logistics_Plan.js
-babel ./GF_logistics_main.js -o ./12.js
-rm ./GF_logistics_main.js
-babel ./GF_logistics_FineTuning.js -o ./13.js
-rm ./GF_logistics_FineTuning.js
-babel ./GF_logistics_MissionTable.js -o ./14.js
-rm ./GF_logistics_MissionTable.js
-babel ./GF_logistics_PlanDetails.js -o ./15.js
-rm ./GF_logistics_PlanDetails.js
-babel ./GF_logistics_Saved.js -o ./16.js
-rm ./GF_logistics_Saved.js
-babel ./GF_logistics_calcTargetValueTool.js -o ./17.js
-rm ./GF_logistics_calcTargetValueTool.js
-rm ./.babelrc
-uglifyjs ./1.js ./2.js ./3.js ./4.js ./5.js ./6.js ./7.js ./8.js ./9.js ./10.js ./11.js ./12.js ./13.js ./14.js ./15.js ./16.js ./17.js -c -m -o ./js/GF_logistics0.min.js --toplevel
-rm ./1.js ./2.js ./3.js ./4.js ./5.js ./6.js ./7.js ./8.js ./9.js ./10.js ./11.js ./12.js ./13.js ./14.js ./15.js ./16.js ./17.js
+# 需要先删去测试代码
+babel ./GF_logistics_.js -o ./js/GF_logistics0.js
+rm ./GF_logistics_.js
+cd ./js
 echo "/**
  * [GF_logistics]{@link https://github.com/CHANTXU64/Girls-Frontline}
  *
  * @namespace GF_logistics
+ * @version 1.0.0
  * @author ChantXu64 [chantxu@outlook.com]
  * @copyright ChantXu64
  * @license MIT
  */
- " > ./js/GF_logistics.min.js
-cat ./js/GF_logistics0.min.js >> ./js/GF_logistics.min.js
-rm ./js/GF_logistics0.min.js
+ " > $jsfile
+ cat GF_logistics0.js >> $jsfile
+ rm GF_logistics0.js
+rm ../.babelrc
+uglifyjs $jsfile -c -m \
+    -o $jsfile_min \
+    --toplevel --source-map url=$jsfile_map\
+    --comments /@license/
 
+#html
+cd ../
 rm ./GF_logistics.html
 cp ../pages/GF_logistics.html ./
-sed -i '12c\    <link href="./dependent/css/GF_logistics_dependent.css" rel="stylesheet">' GF_logistics.html
-sed -i '13c\    <link href="./css/GF_logistics.min.css" rel="stylesheet">' GF_logistics.html
-sed -i '14c\    <script src="./dependent/js/GF_logistics_dependent.min.js"></script>' GF_logistics.html
-sed -i '15c\    <script src="./js/GF_logistics.min.js"></script>' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
-sed -i '16c\' GF_logistics.html
+cssdependent="<!---->\n\t<link\thref=\"./dependent/css/"$cssdependentfile"\"rel=\"stylesheet\">"
+css="<!---->\n\t<link\thref=\"./css/"$cssfile"\"rel=\"stylesheet\">"
+jsdependent="<!---->\n\t<script\tsrc=\"./dependent/js/"$jsdependentfile"\"></script>"
+js="<!---->\n\t<script\tsrc=\"./js/"$jsfile_min"\"></script>"
+sed -i '12,44d' GF_logistics.html
+sed -i 11a$js GF_logistics.html
+sed -i 12c$jsdependent GF_logistics.html
+sed -i 12c$css GF_logistics.html
+sed -i 12c$cssdependent GF_logistics.html
+sed -i '12c\' GF_logistics.html
