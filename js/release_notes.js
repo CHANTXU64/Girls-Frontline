4980db0176b5ea7d054ae41245a462e8bed46ede
CHANTXU64 <chantxu@outlook.com>
2020-06-06 21:06:23 +0800

    fix bug - release notes

--- js/release_notes.js
+++ js/release_notes.js
@@ -35,7 +35,7 @@ function showReleaseNotes() {
 
 function showAllReleaseNotes() {
     let release_notes_length = release_notes.length;
-    Modal.alert(getReleaseNotesHTML(0, release_notes_length - 1), language.JS.release_notes_title);
+    Modal.alert(getReleaseNotesHTML(-1, release_notes_length - 1), language.JS.release_notes_title);
 }
 
 function getReleaseNotesHTML(previous_version_index, current_version_index) {
@@ -50,6 +50,8 @@ function getReleaseNotesHTML(previous_version_index, current_version_index) {
         html += '</h5>';
         html += '<ul style="padding-left: 25px; margin-bottom: 1.5rem;">';
         let notes = release_note.notes[language.version_note_lang];
+        if (notes === undefined)
+            notes = release_note.notes["en"];
         for (let ii = 0; ii < notes.length; ++ii) {
             html += '<li>' + notes[ii] + '</li>';
         }
@@ -65,15 +67,15 @@ const release_notes = [
             "1.0.0-offline"
         ],
         notes: {
+            en: [
+                "Official release"
+            ],
             zh_CN: [
                 "正式发布"
             ],
             zh_TW: [
                 "正式發布"
             ],
-            en: [
-                "Official release"
-            ]
         }
     },
     {
@@ -82,15 +84,15 @@ const release_notes = [
             "1.0.1-offline"
         ],
         notes: {
+            en: [
+                "Fix bugs and improve performance"
+            ],
             zh_CN: [
                 "修复错误并提升性能"
             ],
             zh_TW: [
                 "修復錯誤並提升性能"
             ],
-            en: [
-                "Fix bugs and improve performance"
-            ]
         }
     },
     {
@@ -99,15 +101,15 @@ const release_notes = [
             "1.0.2-offline"
         ],
         notes: {
+            en: [
+                "None"
+            ],
             zh_CN: [
                 "无"
             ],
             zh_TW: [
                 "無"
             ],
-            en: [
-                "None"
-            ]
         }
     },
     {
@@ -116,15 +118,15 @@ const release_notes = [
             "1.0.3-offline"
         ],
         notes: {
+            en: [
+                "None"
+            ],
             zh_CN: [
                 "无"
             ],
             zh_TW: [
                 "無"
             ],
-            en: [
-                "None"
-            ]
         }
     },
     {
@@ -132,6 +134,10 @@ const release_notes = [
             "1.1.0-rc"
         ],
         notes: {
+            en: [
+                "Change style",
+                "Fix bugs and improve performance"
+            ],
             zh_CN: [
                 "更改样式",
                 "修复错误并提升性能"
@@ -140,10 +146,6 @@ const release_notes = [
                 "更改樣式",
                 "修復錯誤並提升性能"
             ],
-            en: [
-                "Change style",
-                "Fix bugs and improve performance"
-            ]
         }
     },
     {
@@ -151,15 +153,15 @@ const release_notes = [
             "1.2.0-rc"
         ],
         notes: {
+            en: [
+                "Plan combination mode"
+            ],
             zh_CN: [
                 "方案组合模式"
             ],
             zh_TW: [
                 "方案組合模式"
             ],
-            en: [
-                "Plan combination mode"
-            ]
         }
     },
     {
@@ -167,6 +169,11 @@ const release_notes = [
             "1.2.0-rc1"
         ],
         notes: {
+            en: [
+                "Chart highlighting",
+                "Click revenue and expenditure plan to apply",
+                "Fix bugs"
+            ],
             zh_CN: [
                 "图表选中高亮",
                 "点击其他收支方案以应用",
@@ -177,11 +184,6 @@ const release_notes = [
                 "點擊其他收支方案以應用",
                 "修復錯誤"
             ],
-            en: [
-                "Chart highlighting",
-                "Click revenue and expenditure plan to apply",
-                "Fix bugs"
-            ]
         }
     },
     {
@@ -189,15 +191,15 @@ const release_notes = [
             "1.2.0-rc2"
         ],
         notes: {
+            en: [
+                "Fix bug"
+            ],
             zh_CN: [
                 "修复错误"
             ],
             zh_TW: [
                 "修復錯誤"
             ],
-            en: [
-                "Fix bug"
-            ]
         }
     },
     {
@@ -206,15 +208,15 @@ const release_notes = [
             "1.2.0-offline"
         ],
         notes: {
+            en: [
+                "Fix some bugs"
+            ],
             zh_CN: [
                 "修复错误"
             ],
             zh_TW: [
                 "修復錯誤"
             ],
-            en: [
-                "Fix some bugs"
-            ]
         }
     },
     {
@@ -223,15 +225,15 @@ const release_notes = [
             "1.2.1-offline"
         ],
         notes: {
+            en: [
+                "Update Chapter 13"
+            ],
             zh_CN: [
                 "更新13章"
             ],
             zh_TW: [
                 "更新13章"
             ],
-            en: [
-                "Update Chapter 13"
-            ]
         }
     },
     {
@@ -239,6 +241,13 @@ const release_notes = [
             "1.3.0-rc",
         ],
         notes: {
+            en: [
+                "Plan combination mode - easy to change date",
+                "Update contract data",
+                "Add release notes",
+                "Various optimizations",
+                "Fix bug"
+            ],
             zh_CN: [
                 "后勤组合排序模式 - 更方便地修改开始结束日期",
                 "更新契约数据",
@@ -253,13 +262,6 @@ const release_notes = [
                 "各種優化",
                 "修復錯誤"
             ],
-            en: [
-                "Plan combination mode - easy to change date",
-                "Update contract data",
-                "Add release notes",
-                "Various optimizations",
-                "Fix bug"
-            ]
         }
     },
 ]
